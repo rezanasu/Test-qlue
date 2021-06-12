01f2db8e2a3234c5daaec438f49f974ce48e52fe
rezanasu <rezanasution82@gmail.com>
2021-06-13 04:00:21 +0700

    algorithmic done

--- algorithmic.js
+++ algorithmic.js
@@ -1,6 +1,25 @@
-function findSums(input) {
+function sumRecursive(target, currentSum, start, output, result) {
+    if(currentSum === target) output.push(result.slice());
 
-    
+    for(let i = start; i < target; i++) {
+        let tempSum = currentSum + i;
+        if(tempSum <= target) {
+            result.push(i);
+            sumRecursive(target, tempSum, i, output, result);
+            result.pop();
+        } else {
+            return;
+        }
+    }
 }
 
 
+function findAllSums(target) {
+    let output = [];
+    let result = [];
+    console.log("n =", target)
+    sumRecursive(target, 0, 1, output, result);
+    return output;
+}
+
+console.log(findAllSums(4));
